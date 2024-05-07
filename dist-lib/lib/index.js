7b8bee6323d945dca4c0fa3dbb0f51ba568482a6
cuiwenke <1045414997@qq.com>
2024-05-07 14:11:05 +0800

    删除多余文件

--- dist-lib/lib/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, Symbol.toStringTag, { value: "Module" });
-const uiButton = require("./uiButton/uiButton.vue.js");
-const uiInput = require("./uiInput/uiInput.vue.js");
-const EventBus = require("./utils/EventBus.js");
-exports.uiButton = uiButton.default;
-exports.uiInput = uiInput.default;
-exports.EventBus = EventBus.default;
